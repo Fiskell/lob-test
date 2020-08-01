9b397cbedad1176692da5997228b000e2fdc6b8a
Fiskell <stephen.fiskell@gmail.com>
2020-08-01 15:01:22 -0700

    API class, made sample request to search addresses endpoint

---
+++ src/api/lobApi.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const lobBaseUrl = "https://api.lob.com/v1/";
+
+const SEARCH_TYPES = {
+  ADDRESSES: "addresses",
+  POST_CARDS: "postcards",
+  LETTERS: "letters",
+  CHECKS: "checks",
+  BANK_ACCOUNTS: "bank_accounts",
+  AREAS: "areas",
+};
+
+export default class lobApi {
+  constructor(
+    username = "test_8ddaad35dc02260ae8a4e6e33d9f3ade7ae",
+    password = ""
+  ) {
+    this.instance = axios.create({
+      baseURL: lobBaseUrl,
+      auth: {
+        username,
+        password,
+      },
+    });
+  }
+
+  // --------- SEARCH ENDPOINTS ------------ //
+  async search(q, types, offset, limit) {
+    return this.instance.get("/search", {
+      params: {
+        q,
+        types,
+        offset,
+        limit,
+      },
+    });
+  }
+
+  async searchAddresses(q, offset = 0, limit = 20) {
+    return this.search(q, SEARCH_TYPES.ADDRESSES, offset, limit);
+  }
+
+  // --------- POST CARD ENDPOINTS --------- //
+  async createPostCard() {}
+}
