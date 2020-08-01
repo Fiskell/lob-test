cfe7083e6a548c476c7d009d30c1741bfc1cddca
Fiskell <stephen.fiskell@gmail.com>
2020-08-01 14:04:05 -0700

    folder structure set up

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "assets/logo.svg";
+import NewPostCardForm from "components/PostCard/NewPostCardForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NewPostCardForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostCard/NewPostCardForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPostCardForm = () => {
+  return <div>form</div>;
+};
+
+export default NewPostCardForm;
