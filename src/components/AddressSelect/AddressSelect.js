45e1328f93cb91f6adce83ee243e475d691fa792
Fiskell <stephen.fiskell@gmail.com>
2020-08-01 15:40:12 -0700

    Address select loading on search

--- src/components/AddressSelect/AddressSelect.js
+++ src/components/AddressSelect/AddressSelect.js
@@ -1,8 +1,107 @@
-import React from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import AsyncSelect from "react-select/async";
+import lobApi from "api/lobApi";
+import _ from "lodash";
 
-const AddressSelect = () => {
-  return <AsyncSelect />;
+const AddressSelect = ({ onChange }) => {
+  const [address, setAddress] = useState(null);
+
+  const styles = {
+    control: (base, state) => ({
+      ...base,
+      minHeight: "50px",
+      display: "flex",
+      alignItems: "center",
+    }),
+    singleValue: (base, state) => ({
+      ...base,
+      minHeight: "50px",
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+    }),
+    valueContainer: (base, state) => ({
+      ...base,
+      display: "flex",
+    }),
+    menu: (base, state) => ({
+      ...base,
+      zIndex: 50,
+    }),
+    group: (base, state) => ({
+      ...base,
+      borderBottom: "1px solid #e5e5e5",
+    }),
+  };
+
+  const loadOptions = (searchValue, callback) => {
+    const api = new lobApi();
+    api.searchAddresses(searchValue).then(
+      (resp) => {
+        let newOptions = [];
+        resp.data.data.map((option) => {
+          newOptions.push({
+            value: option,
+            label: option.id,
+          });
+        });
+        callback(newOptions);
+      },
+      (err) => {
+        console.log("err: ", err);
+      }
+    );
+  };
+
+  // We don't want to search on every key press, only when they stop typing
+  const debounceLoadOptions = useCallback(_.debounce(loadOptions, 500), [
+    address,
+  ]);
+
+  // Callback for form
+  const handleChange = (newAddress) => {
+    setAddress(newAddress ? newAddress.value : null);
+    onChange && onChange(newAddress);
+  };
+
+  return (
+    <AsyncSelect
+      components={{ Option, SingleValue }}
+      styles={styles}
+      placeholder="Search for address..."
+      isClearable
+      cacheOptions
+      loadOptions={debounceLoadOptions}
+      onChange={handleChange}
+    />
+  );
+};
+
+const SingleValue = (props) => {
+  return <Option {...props} />;
+};
+
+const Option = (props) => {
+  const { data, getStyles, innerRef, innerProps } = props;
+  const addr = data.value;
+
+  return (
+    <div
+      {...innerProps}
+      className="cursor-pointer flex flex-grow items-center border-b py-2 px-4 hover:bg-blue-200"
+    >
+      <div className="flex-grow flex flex-col">
+        <div className="text-md mb-1">{addr.name}</div>
+        <div className="flex flex-col text-sm text-blue-900">
+          <div>{addr.address_line1}</div>
+          {addr.address_line2 && <div>{addr.address_line2}</div>}
+          <div>{`${addr.address_city}, ${addr.address_state} ${addr.address_zip}`}</div>
+          <div>{addr.address_country}</div>
+        </div>
+      </div>
+      <div>{addr.id}</div>
+    </div>
+  );
 };
 
 export default AddressSelect;
--- src/components/PostCard/NewPostCardForm.js
+++ src/components/PostCard/NewPostCardForm.js
@@ -1,21 +1,14 @@
 import React, { useEffect } from "react";
 import AddressSelect from "components/AddressSelect/AddressSelect";
 import { FormGroup, FormLabel, FormField, Button } from "components/shared";
-import lobApi from "api/lobApi";
 
 const NewPostCardForm = () => {
-  useEffect(() => {
-    const api = new lobApi();
-    api.searchAddresses("ra").then((resp) => {
-      console.log(resp);
-    });
-  }, []);
   return (
     <div className="border-2 border-blue-200 rounded-lg w-1/2 p-8 shadow-xl bg-white">
       {/* Description */}
       <FormGroup>
         <FormLabel>Description:</FormLabel>
-        <FormField value={""} autofocus />
+        <FormField value={""} autoFocus />
       </FormGroup>
 
       {/* To address */}
